e301a526003d224f21f16153222c31a6afbd37b6
Marianne Oliveira <mariannerodrigues0806@gmail.com>
2025-03-21 22:25:07 -0300

    Aula sobre firebase faltando terminar o código

--- aula04/app/index.tsx
+++ aula04/app/index.tsx
@@ -1,15 +1,55 @@
-import { Text, View } from "react-native";
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export default function Index() {
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJy88h-X1nEYRuK0rEHUO-8yUJG4bc75M",
+  authDomain: "meu-primeiro-firebase-e9077.firebaseapp.com",
+  projectId: "meu-primeiro-firebase-e9077",
+  storageBucket: "meu-primeiro-firebase-e9077.firebasestorage.app",
+  messagingSenderId: "299924125902",
+  appId: "1:299924125902:web:c0d3fa3720f2fb05fdf89f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+export default function App() {
+  const [nomes, setNomes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const nomesCollection = firebase.firestore().collection('Nomes');
+      const snapshot = await nomesCollection.get();
+
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+
+      setNomes(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <View style={{ flex: 1, justifyContent: 'center',
+    alignItems: 'center' }}>
+      <Text>Lista de Nomes:</Text>
+      <FlatList
+      data={nomes}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View>
+          <Text></Text>
+        </View>
+      )}
+  )
 }
